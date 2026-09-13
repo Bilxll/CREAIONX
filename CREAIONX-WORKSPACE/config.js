2fd55a687cef333db95db7bf9631b280beadcabb
Muhammad Bilal Siddiqui <muhxmmxd.bilxl@gmail.com>
2026-09-13 08:41:21 +0500

    Connect Workspace frontend to live Apps Script API

--- CREAIONX-WORKSPACE/config.js
+++ CREAIONX-WORKSPACE/config.js
@@ -1,3 +1,3 @@
 window.CX_CONFIG = {
-  APPS_SCRIPT_URL: ""
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxWZLpE9APX0G9K1YPSFknwt_uHuuWaRQB96I28x3wlSTaAv6qb6vV6Go7iOyeeDWffTA/exec"
 };
